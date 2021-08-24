d840bdcaebd3baa18e6e2de7ebb1b382f0d0a170
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-08-23 20:18:37 -0500

    Update exercise.py

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+print("Ending balance of a bank account")
+
+Blnc = float(input("Input the balance of the previous month:"))
+In = float(input("Input the income:"))
+Out = float(input("Input the outcome:"))
+Checks = (int(input("Input the number of checks:"))*13)
+MBlnc=Blnc+In-Out-Checks
+BlncF=MBlnc-(MBlnc*0.075)
+
+print("The balance of the account is:",BlncF)
 
 if __name__ == '__main__':
     main()
