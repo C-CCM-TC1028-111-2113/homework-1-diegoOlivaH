23f79f56a376fcff29c7009689647a81e6fbba3b
diegoOlivaH <88850984+diegoOlivaH@users.noreply.github.com>
2021-08-23 19:30:07 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,8 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
     #Leer los datos
-    pass
+print("Monthly Telefonic Fee Calculator")
 
+msj = int(input("Input the number of messages:"))
+mgb = float(input("Input the number of megas:"))
+mint = int(input("Input the number of minutes:"))
+
+Mensual = (msj+mgb+mint)*0.8
+
+print("The monthly fee will be of:", Mensual)
 
 if __name__ == '__main__':
     main()
